245cd8eadc3c21cb124236bb731d3397b2f9203d
Luigi Lupini <luigi.lupini@gmail.com>
2022-07-14 11:50:06 +0200

    updated

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const hexTitles = document.querySelectorAll(".color h2");
 let initialColors;
-console.log();
 
 // FUNCTIONS
 // custom color generator:
@@ -28,18 +27,50 @@ function randomColors() {
     hexTitle.innerText = randomColor;
     // check for contrast:
     checkTextContrast(randomColor, hexTitle);
+    // slider configuration:
+    const sliders = div.querySelectorAll(".sliders input");
+    const [hue, brightness, saturation] = sliders;
+    colorizeSliders(randomColor, hue, brightness, saturation);
   });
 }
 
 function checkTextContrast(color, text) {
   const luminance = chroma(color).luminance();
   if (luminance > 0.5) {
-    text.style.color = color.darken(2);
-    text.style.textShadow = `1px 1px 2px ${color.darken(1)}`;
+    text.style.color = "black";
+    // text.style.color = color.darken(2);
+    // text.style.textShadow = `1px 1px 1px ${color.darken(1)}`;
   } else {
-    text.style.color = color.brighten(2);
-    text.style.textShadow = `1px 1px 2px ${color.brighten(1)}`;
+    text.style.color = "white";
+    // text.style.color = color.brighten(2);
+    // text.style.textShadow = `1px 1px 1px ${color.brighten(1)}`;
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  // brightness:
+  const mid_bright = color.set("hsl.l", 0.5);
+  const scale_bright = chroma.scale(["black", mid_bright, "white"]);
+  // saturation:
+  const no_sat = color.set("hsl.s", 0);
+  const full_sat = color.set("hsl.s", 1);
+  const scale_sat = chroma.scale([no_sat, color, full_sat]);
+
+  // update input with scaling gradients:
+  hue.style.backgroundImage = `linear-gradient(to right,
+    rgb(204, 75, 75),
+    rgb(204, 204, 75),
+    rgb(75, 204, 75),
+    rgb(75, 204, 204),
+    rgb(75, 75, 204),
+    rgb(204, 75, 204),
+    rgb(204, 75, 75)
+  )`;
+  saturation.style.backgroundImage = `linear-gradient(to right,
+    ${scale_sat(0)}, ${scale_sat(1)})`;
+  brightness.style.backgroundImage = `linear-gradient(to right,
+    ${scale_bright(0)}, ${scale_bright(0.5)}, ${scale_bright(1)}
+  )`;
 }
 
 randomColors();
